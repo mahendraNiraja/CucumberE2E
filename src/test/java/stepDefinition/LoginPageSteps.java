e8d94691663c7eed4dad285c227ce2143a2402e1
mahee <kaushik.ku90@gmail.com>
2022-03-30 19:40:26 +0530

    updated

---
+++ src/test/java/stepDefinition/LoginPageSteps.java
@@ -0,0 +1,53 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginPageSteps {
+
+
+	@Given("User is in login Page")
+	public void user_is_in_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User get Title of Page")
+	public void user_get_title_of_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Page title shoul be {string}")
+	public void page_title_shoul_be(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("Login page title")
+	public void login_page_title() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User enter Username")
+	public void user_enter_username() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user enter password")
+	public void user_enter_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Click on login button")
+	public void click_on_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
--- src/test/java/testRunner/MyRunner.java
+++ src/test/java/testRunner/MyRunner.java
@@ -1,5 +1,22 @@
 package testRunner;
 
-public class MyRunner {
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions
+		(features = {"src/test/resourses/features/LoginPage.feature"},
+		glue= {"stepDefinition","appHooks"},
+		plugin = {"pretty", "html:test-output", "json:target/JsonReports/Cucumber-Report.json", "junit:junit_xml/cucumber.xml"},
+		monochrome = true,
+		strict = true,
+		dryRun=false)
+		//tags={"@CRMLoginTest"})
+		//tags={"@SanityTest, @RegressionTest"})
+
+public class MyRunner
+{
 
 }
