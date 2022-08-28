40ed2e2fe9d9915011791936a9762a28e72bb989
mahee <kaushik.ku90@gmail.com>
2022-08-28 14:29:31 +0530

    implement logger

--- src/main/java/com/utils/ElementUitil.java
+++ src/main/java/com/utils/ElementUitil.java
@@ -1,14 +1,24 @@
 package com.utils;
 
+
 import org.openqa.selenium.WebElement;
+import org.testng.log4testng.Logger;
+
 
 public class ElementUitil {
+	
+	
+	static org.apache.log4j.Logger logger=org.apache.log4j.Logger.getLogger(ElementUitil.class);
+
 
 	public static void doClick(WebElement ele) {
 		try {
 			ele.click();
+			logger.info("User is able to perform click operation");
+		
 		} catch (Exception e) {
 			e.printStackTrace();
+			logger.warn("User is not able to perform click operation");
 		}
 
 	}
